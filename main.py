6d24e2e9e863a6483c7ee7a3fd26197b821da47f
Johnny-kiv <johnny.kiv@yandex.ru>
2023-01-28 18:59:52 +0300

    3

--- main.py
+++ main.py
@@ -7,11 +7,13 @@ token = "Pogodaintheworld_bot"
 
 main_m = types.ReplyKeyboardMarkup()
 add_city = types.KeyboardButton(text="Добавить город")
+add_city = types.KeyboardButton(text="Посмотреть города")
+main_m.add(add_city)
 
 bot = Bot(token=token)
 dp = Dispatcher()
 
-
 async def message_hadler(mes: types.Message):
+    user = mes.from_user.id
     if mes == "Добавить город":
         pass
--- sql.py
+++ sql.py
@@ -1,2 +1,27 @@
-def addInBaze():
-    pass
+import sqlite3
+import requests
+from bs4 import BeautifulSoup
+
+conn = sqlite3.connect("pogoda")
+cur = conn.cursor()
+
+def addInBaze(ind,name,lat,lon):
+    cur.execute(f"INSERT INTO cities (nom, name, lat,lon) VALUES ({l}, {name}, {cord[0]},{cord[1]});")
+"""def select_name(name):
+    cur.execute(f"SELECT name FROM cities WHERE name = {name}")
+    result = cur.fetchall()
+    return result"""
+src = BeautifulSoup(requests.get("https://time-in.ru/coordinates").text)
+countries= src.find("ul",class_="coordinates-list").find_all("li")
+l = 0
+for country in countries:
+    urlc = country.find("a").get("href")
+    url = BeautifulSoup(requests.get(f"{urlc}").text)
+    cities = url.find("ul",class_="coordinates-items").find_all("li")
+    for city in cities:
+        l+=1
+        name = city.find("a",class_="coordinates-items-left").text
+        cord = city.find("div",class_="coordinates-items-right").text.split(",")
+        #addInBaze(l,name,float(cord[0]),float(cord[1]))
+        cur.execute(f"INSERT INTO cities (nom, name, lat,lon) VALUES ({l}, {name}, {cord[0]},{cord[1]});")
+
