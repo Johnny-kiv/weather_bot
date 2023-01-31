1033824b36c61d3be5610010bfa7888f765daac3
Johnny-kiv <johnny.kiv@yandex.ru>
2023-01-31 17:37:32 +0300

    3

--- main.py
+++ main.py
@@ -1,24 +1,25 @@
-from aiogram import Bot,Dispatcher,types
+from aiogram import Bot,Dispatcher,types,executor
 import asyncio
-from sql import addInBaze
 
 
-token = "Pogodaintheworld_bot"
+token = "5964365949:AAHCk4qPSpRkhLEkZ10CjPswy_gpBeb2ksk"
 
 main_m = types.ReplyKeyboardMarkup()
 add_city = types.KeyboardButton(text="Добавить город")
 look_city = types.KeyboardButton(text="Посмотреть города")
-main_m.add(add_city)
+main_m.add(add_city,look_city)
 
 bot = Bot(token=token)
-dp = Dispatcher()
+dp = Dispatcher(bot=bot)
 
+@dp.message_handler(commands=['start'])
 async def message_hadler(mes: types.Message):
-    if mes == "Добавить город":
-        await mes.answer(text="Подождите...")
+    await bot.send_message(mes.from_user.id,text="Привет",reply_markup=main_m)
 
-async def main():
-    await dp.start_polling(bot)
+@dp.message_handler()
+async def message_hadler(mes: types.Message):
+    if mes.text == "Добавить город":
+        await bot.send_message(mes.from_user.id,text="Подождите...",reply_markup=main_m)
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    executor.start_polling(dp,skip_updates=True)
