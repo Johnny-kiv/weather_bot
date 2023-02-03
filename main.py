d1dba3941d2a30ae78192c088b5917fda8a8ef03
Johnny-kiv <johnny.kiv@yandex.ru>
2023-02-03 22:58:21 +0300

    3

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from aiogram import Bot,Dispatcher,types,executor
-import asyncio
+import sqlite3
 
+conn = sqlite3.connect("pogoda")
+cur = conn.cursor()
 
 token = "5964365949:AAHCk4qPSpRkhLEkZ10CjPswy_gpBeb2ksk"
 
@@ -19,7 +21,14 @@ async def message_hadler(mes: types.Message):
 @dp.message_handler()
 async def message_hadler(mes: types.Message):
     if mes.text == "Добавить город":
-        await bot.send_message(mes.from_user.id,text="Подождите...",reply_markup=main_m)
+        await bot.send_message(mes.from_user.id,text="Напишите название города",reply_markup=main_m)
+        choice_city(mes=mes.text)
+def choice_city(mes):
+    cur.execute(f"SELECT lat,lon from cities WHERE name = '{mes};'")
+    res = cur.fetchall()
+    print(res)
+    #ot.send_message(mes.from_user.id, text=res, reply_markup=main_m)
+
 
 if __name__ == "__main__":
     executor.start_polling(dp,skip_updates=True)
---
+++ test.py
@@ -0,0 +1,30 @@
+import requests
+import random
+from bs4 import BeautifulSoup as bs
+
+def get_free_proxies():
+    url = "https://free-proxy-list.net/"
+    # get the HTTP response and construct soup object
+    soup = bs(requests.get(url).content, "html.parser")
+    proxies = []
+    for row in soup.find("table").find_all("tr")[1:]:
+        tds = row.find_all("td")
+        try:
+            ip = tds[0].text.strip()
+            port = tds[1].text.strip()
+            host = f"{ip}:{port}"
+            proxies.append(host)
+        except IndexError:
+            continue
+    return proxies
+proxies = get_free_proxies()
+def get_session(proxies):
+    # construct an HTTP session
+    session = requests.Session()
+    # choose one random proxy
+    proxy = random.choice(proxies)
+    session.proxies = {"http": proxy, "https": proxy}
+    return session
+for i in range(5):
+    s = get_session(proxies)
+    print("Request page with IP:", s.get("https://yandex.ru/pogoda/?lat=59.5&lon=30", timeout=1.5).text.strip())
