81c357780b77403b76ac8f99a4446d3352666cc0
Johnny-kiv <johnny.kiv@yandex.ru>
2023-02-07 21:05:06 +0300

    3

--- main.py
+++ main.py
@@ -7,17 +7,11 @@ conn = sqlite3.connect("pogoda")
 cur = conn.cursor()
 
 token = "5964365949:AAHCk4qPSpRkhLEkZ10CjPswy_gpBeb2ksk"
-
-main_m = types.ReplyKeyboardMarkup()
-add_city = types.KeyboardButton(text="Добавить город")
-look_city = types.KeyboardButton(text="Посмотреть города")
-main_m.add(add_city,look_city)
-
 bot = Bot(token=token)
 dp = Dispatcher(bot=bot)
 @dp.message_handler(commands=['start'])
 async def message_hadler(mes: types.Message):
-    await bot.send_message(mes.from_user.id,text="Привет! Напиши город",reply_markup=main_m)
+    await bot.send_message(mes.from_user.id,text="Привет! Напиши город")
 @dp.message_handler()
 async def message_hadler(mes: types.Message):
     src = bs4.BeautifulSoup(requests.get(f"https://translate.yandex.ru/?from=tableau_yabro&source_lang=ru&target_lang=en&text={mes}").text)
