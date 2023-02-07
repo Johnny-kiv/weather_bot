b9101a6423e05a6749c339739427cdb20f5b7853
Johnny-kiv <johnny.kiv@yandex.ru>
2023-02-07 21:06:24 +0300

    3

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ async def message_hadler(mes: types.Message):
     await bot.send_message(mes.from_user.id,text="Привет! Напиши город")
 @dp.message_handler()
 async def message_hadler(mes: types.Message):
-    src = bs4.BeautifulSoup(requests.get(f"https://translate.yandex.ru/?from=tableau_yabro&source_lang=ru&target_lang=en&text={mes}").text)
+    src = bs4.BeautifulSoup(requests.get(f"https://translate.yandex.ru/?from=tableau_yabro&source_lang=ru&target_lang=en&text={mes.text}").text)
     await bot.send_message(mes.from_user.id,str(src))
 
 
